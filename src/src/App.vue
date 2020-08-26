9120705387a8f778a2725462247406449cd3c07f
Alex Hampu <alex.hampu@deploy.com.ro>
2020-08-26 11:18:54 +0300

    add vue router

--- src/src/App.vue
+++ src/src/App.vue
@@ -2,20 +2,15 @@
   <div id="app">
     <Header></Header>
     <main>
-      <div class="container mx-auto text-center my-10">
-        <div class="flex justify-center text-4xl">
-          <div class="mx-4"><a href="https://github.com/alexhampu" class="hover:text-blue-700"><i class="fab fa-github"></i> Github</a></div>
-          <div class="mx-4"><a href="https://linkedin.com/in/alexhampu95" class="hover:text-blue-700"><i class="fab fa-linkedin"></i> LinkedIn</a></div>
-        </div>
-      </div>
+      <router-view></router-view>
     </main>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Footer from "@/components/Footer";
+import Header from '@/components/Header'
+import Footer from '@/components/Footer';
 
 export default {
   name: 'App',
---
+++ src/src/views/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container mx-auto text-center my-10">
+    <div class="flex justify-center text-4xl">
+      <div class="mx-4"><a href="https://github.com/alexhampu" class="hover:text-blue-700"><i class="fab fa-github"></i>
+        Github</a></div>
+      <div class="mx-4"><a href="https://linkedin.com/in/alexhampu95" class="hover:text-blue-700"><i
+          class="fab fa-linkedin"></i> LinkedIn</a></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped>
+
+</style>
