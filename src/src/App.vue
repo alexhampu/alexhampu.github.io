69998801a6bdec1b3c1041a0ccb27e94ec2156ec
Alex Hampu <alex.hampu@deploy.com.ro>
2020-08-26 09:51:37 +0300

    add contact information

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="app">
     <Header></Header>
-    <div class="container mx-auto text-center my-10">
-      <p>Coming soon</p>
-    </div>
+    <main>
+      <div class="container mx-auto text-center my-10">
+        <div class="flex justify-center text-4xl">
+          <div class="mx-4"><a href="https://github.com/alexhampu" class="hover:text-blue-700"><i class="fab fa-github"></i> Github</a></div>
+          <div class="mx-4"><a href="https://linkedin.com/in/alexhampu95" class="hover:text-blue-700"><i class="fab fa-linkedin"></i> LinkedIn</a></div>
+        </div>
+      </div>
+    </main>
     <Footer></Footer>
   </div>
 </template>
